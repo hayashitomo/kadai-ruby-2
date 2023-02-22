815ee1ad29e5450f605f2f03a57aac7201a5172a
hayashi.tomoyuki <nakamaruhayashi@gmail.com>
2023-02-22 12:59:34 +0000

    kadaitotyu2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,19 @@
-num_max = 100 #どこに入力すればいいかわかりません
+num_max = 100 #num_maxは100
 
-
-def fizzbuzz(num)    　#メソッドFizzBuzzを定義しろ
-  result = 0    #戻り値をいれる箱を作れ
-  (1..100).each do |number　#1～100まで繰り返せ
-  result = number　#???
-  if number % 3 =0 #もし3で割り切れるならFizzと出力しろ
-    puts "Fizz"
-  elsif number % 5 =0　#もし5で割り切れるならBuzzと出力しろ
-    puts "Buzz"
-  elsif number % 15 = 0　#もし15で割り切れるならFizzBuzzと出力しろ
-    puts "FizzBuzz"
-  else
-    puts number　#上記以外なら数字を出力しろ
-  end
- return result　#???
+def fizzbuzz(num)　#FizzBuzzメソットを定義しろ
+  result = 0　#結果を反映するresultを作成しろ
+  result += number　#???
+  (1..num_max).each do |number|　#1～100まで順番にnumberに整数を代入しろ
+      if number / 3 == 0　#3で割り切れればFizzと出力しろ
+      puts "Fizz"
+    elsif number / 5 == 0　#5で割り切れればBuzzと出力しろ
+      puts "Buzz"
+    elsif number /15 == 0　#15で割り切れればFizzBuzzと出力しろ
+      puts "FizzBuzz"
+    else 
+      puts number　#上記以外なら数字を出力しろ
+    end
+  return result
 end
+end
+puts fizzbuzz(num)
