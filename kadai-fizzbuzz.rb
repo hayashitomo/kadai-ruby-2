5efe76bbb026ed53e718e42587743e03f6a7477b
hayashi.tomoyuki <nakamaruhayashi@gmail.com>
2023-02-23 12:59:33 +0000

    kadai20230223ver2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,18 @@
-num_max = 100
+num_max =100
 
 def fizzbuzz(num)
   if num % 15 == 0
     return "FizzBuzz"
-    
-  elsif num % 3 ==0
-    return "Fizz"
-    
-  elsif num % 5 == 0
+  elsif % 3 == 0
+    return "fizz"
+  elsif % 5 == 0
     return "Buzz"
-    
-  else num
+  else 
+    return num
   end
 end
 
-(1..num_max).each do |num|
-  puts fizzbuzz(num)
-end
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
